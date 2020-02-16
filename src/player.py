7babb1bfbaf5ad1fef7c15a58fcd54dc465879c7
Logan Karney <karneyl@mail.gvsu>
2020-02-16 14:41:06 -0500

    Added list that stores fired arrows

--- src/player.py
+++ src/player.py
@@ -44,6 +44,9 @@ class Player(Character):
 
         self.arrow_image = pygame.image.load('../assets/arrow.png').convert_alpha()
 
+        # Contains list of fired arrows in this scene
+        self.arrows = []
+
         ## Walk sprites
         self.walk_length = 9
         raw_walk_sprites = Spritesheet('../assets/plate_walk.png', 64, self.walk_length)
@@ -208,9 +211,7 @@ class Player(Character):
 
             if (self.current_frame is 9):
                 arrow = Arrow(self.arrow_image, self.direction, self.engine, 3,self.x + 31,self.y + 31)
-                print('length of engine objects is: ')
-                print(len(self.engine.objects))
-                print()
+                self.arrows.append(arrow)
 
             if (self.current_frame is 12):
                 self.current_frame = 3
